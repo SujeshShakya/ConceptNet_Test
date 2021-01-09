2ff68f34438e418fb8ac70a028e993b53550bec7
sujesh <sujeshmanshakya@gmail.com>
2021-01-10 10:14:42 +1300

    Added validations and formatted json to string with some removed comments

--- nodes.js
+++ nodes.js
@@ -2,18 +2,17 @@ const axios = require('axios');
 const json = {};
 const term = process.argv[2];
 getByTerms(term).then(result =>{
-    var label1 = result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label;
-    // json[label1] = {};
-    // json.push(result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label);
+    if(result.edges.length != 0){
+        var label1 = result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label;
+    }
    getByTerms(label1).then(resp =>{
-       var label2 = resp.edges.sort((a, b)=> b.weight - a.weight)[0].end.label;
-    json[label2] = {};
-    json[label2][result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label] = {};
-    json[label2][result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label][term] = {};
-
-    // json.push(resp.edges.sort((a, b)=> b.weight - a.weight)[0].end.label);
-    // json.push(term);
-    console.log(json)
+    if(resp.edges.length != 0){   
+        var label2 = resp.edges.sort((a, b)=> b.weight - a.weight)[0].end.label;
+        json[label2] = {};
+        json[label2][result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label] = {};
+        json[label2][result.edges.sort((a, b)=> b.weight - a.weight)[0].end.label][term] = {};
+    }
+    console.log(JSON.stringify(json, null, 2))
    })
 })
 
